201cc418fcf3e06e4711332bc0073db04436bd9e
Johann Wimmer <johann.wimmer@gmail.com>
2020-10-26 11:23:29 +0100

    Intial commit

---
+++ src/UpdateGenerator/Generator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateGenerator
+{
+    [Generator]
+    public class UpdateGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var sourceText = SourceText.From("[assembly: System.Reflection.AssemblyMetadata(\"Setup\", \"is working\")]", Encoding.UTF8);
+            context.AddSource("Generated.cs", sourceText);
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
